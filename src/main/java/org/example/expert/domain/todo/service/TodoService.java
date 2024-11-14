90548cb95453ea7976d2f06890d92fe88c229f35
hubaek <zjfl3122@naver.com>
2024-11-14 21:48:24 +0900

    Feat : saveTodo 조건 추가 JPQL

--- src/main/java/org/example/expert/domain/todo/service/TodoService.java
+++ src/main/java/org/example/expert/domain/todo/service/TodoService.java
@@ -17,6 +17,8 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
+
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
@@ -48,10 +50,12 @@ public class TodoService {
         );
     }
 
-    public Page<TodoResponse> getTodos(int page, int size) {
+    public Page<TodoResponse> getTodos(int page, int size, LocalDateTime startDate, LocalDateTime endDate) {
         Pageable pageable = PageRequest.of(page - 1, size);
 
-        Page<Todo> todos = todoRepository.findAllByOrderByModifiedAtDesc(pageable);
+        String weather = weatherClient.getTodayWeather();
+
+        Page<Todo> todos = todoRepository.findTodosWithFilters(weather, startDate, endDate , pageable);
 
         return todos.map(todo -> new TodoResponse(
                 todo.getId(),
