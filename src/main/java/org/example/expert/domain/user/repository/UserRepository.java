e69b2ad68f4be28afee813f1d27a06166fbad1ff
hubaek <zjfl3122@naver.com>
2024-11-21 12:13:05 +0900

    Feat : 유저 캐싱 기능 추가

--- src/main/java/org/example/expert/domain/user/repository/UserRepository.java
+++ src/main/java/org/example/expert/domain/user/repository/UserRepository.java
@@ -1,13 +1,17 @@
 package org.example.expert.domain.user.repository;
 
 import org.example.expert.domain.user.entity.User;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
+@CacheConfig(cacheNames = "users")
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
     boolean existsByEmail(String email);
 
+    @Cacheable(key = "#nickname", unless = "#result == null ")
     User findByNickname(String nickname);
 }
--- src/main/java/org/example/expert/domain/user/service/UserService.java
+++ src/main/java/org/example/expert/domain/user/service/UserService.java
@@ -12,7 +12,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
-@Transactional(readOnly = true)
+//@Transactional(readOnly = true)
 public class UserService {
 
     private final UserRepository userRepository;
